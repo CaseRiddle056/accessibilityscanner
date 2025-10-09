15b6b4c8a339500da9e0ea0bf688e97cc2344d08
CaseRiddle056 <132170214+CaseRiddle056@users.noreply.github.com>
2025-10-09 13:25:44 -0400

    Manifest changes to accomodate API

--- content/content.js
+++ content/content.js
@@ -1,2 +1,16 @@
-// Runs in the context of web pages
-console.log('Accessibility Scanner content script loaded.');
+// content.js
+(async () => {
+  // Load axe-core script
+  const script = document.createElement("script");
+  script.src = chrome.runtime.getURL("vendor/axe.min.js");
+  script.onload = async () => {
+    // Run axe after it loads
+    window.axe.run(document, {}, (err, results) => {
+      if (err) throw err;
+
+      console.log("Accessibility Violations Found:");
+      console.log(results.violations);
+    });
+  };
+  document.head.appendChild(script);
+})();
